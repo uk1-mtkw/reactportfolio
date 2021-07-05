fd6ce64525289188102d9796fc115f4f7ea4e9aa
uki <uk1.mtkw@gmail.com>
2021-07-05 15:20:44 +0900

    bug fix

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
-import Fade from "react-reveal";
 
 class Footer extends Component {
   render() {
     if (!this.props.data) return null;
 
-    const networks = this.props.data.social.map(function (network) {
-      return (
-        <li key={network.name}>
-          <a href={network.url}>
-            <i className={network.className}></i>
-          </a>
-        </li>
-      );
-    });
+    // const networks = this.props.data.social.map(function (network) {
+    //   return (
+    //     <li key={network.name}>
+    //       <a href={network.url}>
+    //         <i className={network.className}></i>
+    //       </a>
+    //     </li>
+    //   );
+    // });
 
     return (
       <footer>
